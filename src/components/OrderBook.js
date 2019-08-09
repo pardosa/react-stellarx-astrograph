231d2dc52b0a3361966c68833b0d832b517b16ad
Harry .P <harry2604@gmail.com>
2019-08-09 09:29:48 +0800

    update subscription to use state

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import gql from 'graphql-tag';
 import {Query, Subscription} from 'react-apollo';
 import Order from './Order'
@@ -49,81 +49,91 @@ const SUBSCRIPTION = gql`
     }
 `;
 
-export class OrderBook extends Component {
-    render() {
-        return (
-            <div className="m-3">
-                <h4>ORDER BOOK</h4>
-                <Query query={ORDER_QUERY}>
-                    {
-                        ({loading, error, data}) => {
-                            if (loading) return <div className="loading">loading...</div>;
-                            if (error) {
-                                console.log(error);
-                                return (<span>Error!</span>);
-                            }
 
-                            let sum = 0;
-                            
-                            return <div className="table-responsive overflow-auto" style={{
-                                position: 'relative',
-                                height: '250px',
-                                overflow: 'scroll'
-                              }}>
-                                <div className="row table-dark">
-                                    <div className="col-4 text-right">SIZE (XLM)</div>
-                                    <div className="col-3 text-right">SUM</div>
-                                    <div className="col-5 text-right">PRICE(ETH)</div>
-                                </div>
-                                <div className="row table-active">
-                                    <div className="col-4 text-right">Bids:</div>
-                                </div>
-                                {
-                                    data.orderBook.bids.map(function(bid, i){
-                                        sum = bid.amount;
-                                        return <Order 
-                                            key={i}
-                                            size={bid.amount}
-                                            sum={sum}
-                                            price={bid.price}
-                                            />
-                                    })
-                                }
-                                <div className="row table-active">
-                                    <div className="col-4 text-right">Asks:</div>
-                                </div>
-                                {
-                                    data.orderBook.asks.map(function(bid, i){
-                                        sum = bid.amount;
-                                        return <Order 
-                                            key={i}
-                                            size={bid.amount}
-                                            sum={sum}
-                                            price={bid.price}
-                                            />
-                                    })
-                                }
+const OrderBook = () => {
+
+    const [dataBids, setDataBids] = useState([]);
+    const [dataAsks, setDataAsks] = useState([]);
+
+    
+    return (
+        <div className="m-3">
+            <h4>ORDER BOOK</h4>
+            <Query query={ORDER_QUERY}>
+                {
+                    ({loading, error, data}) => {
+                        if (loading) return <div className="loading">loading...</div>;
+                        if (error) {
+                            console.log(error);
+                            return (<span>Error!</span>);
+                        }
+
+                        let sum = 0;
+                        setDataBids(data.orderBook.bids);
+                        setDataAsks(data.orderBook.asks);
+                        
+                        return <div className="table-responsive overflow-auto" style={{
+                            position: 'relative',
+                            height: '250px',
+                            overflow: 'scroll'
+                            }}>
+                            <div className="row table-dark">
+                                <div className="col-4 text-right">SIZE (XLM)</div>
+                                <div className="col-3 text-right">SUM</div>
+                                <div className="col-5 text-right">PRICE(ETH)</div>
                             </div>
-                          ;
-                        }
-                    }
-                </Query>
-                <Subscription subscription={SUBSCRIPTION}>
-                    {
-                        ({loading, error, data}) => {
-                            if (loading) return <div className="loading">loading...</div>;
-                            if (error) {
-                                console.log(error);
-                                return (<span>Error!</span>);
+                            <div className="row table-active">
+                                <div className="col-4 text-right">Bids:</div>
+                            </div>
+                            {
+                                dataBids.map(function(bid, i){
+                                    sum = bid.amount;
+                                    return <Order 
+                                        key={i}
+                                        size={bid.amount}
+                                        sum={sum}
+                                        price={bid.price}
+                                        />
+                                })
                             }
-                            console.log(data);
-                            return null;
-                        }
+                            <div className="row table-active">
+                                <div className="col-4 text-right">Asks:</div>
+                            </div>
+                            {
+                                dataAsks.map(function(bid, i){
+                                    sum = bid.amount;
+                                    return <Order 
+                                        key={i}
+                                        size={bid.amount}
+                                        sum={sum}
+                                        price={bid.price}
+                                        />
+                                })
+                            }
+                        </div>
+                        ;
                     }
-                </Subscription>
-            </div>
-        )
-    }
+                }
+            </Query>
+            <Subscription subscription={SUBSCRIPTION}>
+                {
+                    ({loading, error, data}) => {
+                        if (loading) return <div className="loading">loading...</div>;
+                        if (error) {
+                            console.log(error);
+                            return (<span>Error!</span>);
+                        }
+                        console.log(data);
+                        if (data.offer !== null){
+                            
+                        }
+                        
+                        return null;
+                    }
+                }
+            </Subscription>
+        </div>
+    )
 }
 
 export default OrderBook
